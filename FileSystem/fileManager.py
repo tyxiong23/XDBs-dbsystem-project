9f89ad89068c827f37edbb19a067ed352e087430
Tianyi Xiong <x-eric2019@hotmail.com>
2023-01-04 11:29:02 +0800

    add file system

---
+++ FileSystem/fileManager.py
@@ -0,0 +1,48 @@
+from utils.constant import *
+import numpy as np
+import os
+
+class FileManager:
+    def __init__(self) -> None:
+        # self.fd = np.zeros(MAX_FILE_NUM, dtype=int)
+        self.file_dict = dict()
+        self.OPEN_FILE_MOD = os.O_RDWR
+
+    def create_file(self, name: str):
+        f = open(name, "w")
+        assert f is not None, f"Fail to create file [{name}]!!"
+        f.close()
+        pass
+    
+    def open_file(self, name: str):
+        fileID: int = os.open(name, self.OPEN_FILE_MOD)
+        assert fileID != -1, f"Cannot open file [{name}]"
+        return fileID
+
+    def remove_file(self, name: str):
+        os.remove(name)
+
+    def write_page(self, fileID: int, pageID: int, buf: np.ndarray):
+        os.lseek(fileID, pageID << PAGE_SIZE_BITS, os.SEEK_SET)
+        os.write(fileID, buf.tobytes())
+
+    def new_page(self, fileID: int, buf: np.ndarray):
+        offset = os.lseek(fileID, 0, os.SEEK_END)
+        bt_buffers = buf.tobytes()
+        os.write(fileID, bt_buffers)
+        return offset >> PAGE_SIZE_BITS
+
+    def read_page(self, fileID: int, pageID: int):
+        os.lseek(fileID, pageID << PAGE_SIZE_BITS, os.SEEK_SET)
+        error = os.read(fileID, PAGE_SIZE)
+        assert error is not None, f"Cannot read page {pageID} in file {fileID}!!"
+        return error
+
+    def close_file(self, fileID: int):
+        os.close(fileID)
+    
+    def move_file(self, src: str, dst: str):
+        os.rename(src, dst)
+
+    def file_exists(self, name: str):
+        return True if os.path.exists(name) else False
---
+++ FileSystem/findReplace.py
@@ -0,0 +1,26 @@
+from .linkList import MyLinkList
+
+"""
+基于自己实现的linkedList
+完成了LRU替换算法
+"""
+
+class FindReplace:
+    def __init__(self, cap: int) -> None:
+        self.capacity = cap
+        self.linkedList = MyLinkList(cap, 1)
+        for idx in range(0, self.capacity):
+            self.linkedList.insert_first(0, self.capacity - 1 - idx)
+        
+    def find(self):
+        idx = self.linkedList.get_first(0)
+        self.linkedList.delete(idx)
+        self.linkedList.insert(0, idx)
+        return idx
+
+    def access(self, idx: int):
+        self.linkedList.insert(0, idx)
+    
+    def free(self, idx: int):
+        self.linkedList.insert_first(0, idx)
+        
