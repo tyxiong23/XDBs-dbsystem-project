7e9323cdf3abe7766c71d901112d515799f653cd
Tianyi Xiong <x-eric2019@hotmail.com>
2023-01-15 21:34:21 +0800

    modify main.py

--- utils/options.py
+++ utils/options.py
@@ -3,12 +3,11 @@ from pathlib import Path
 
 def getArgs():
     parser = ArgumentParser("XDB 2023")
-    choices = ["table", "csv"]
-    parser.add_argument('-p', '--printer', type=str, choices=choices, default='table')
-    parser.add_argument('-b', '--base-dir', type=Path, default='data')
-    parser.add_argument('-f', '--file', type=Path)
-    parser.add_argument('-t', '--table', type=str)
-    parser.add_argument('database', nargs='?', type=str)
+    parser.add_argument('-b', '--base-dir', type=Path, default='data', help="root path of stored data for XDB")
+    parser.add_argument('-f', '--file', type=Path, help="input file with [csv/sql] format")
+    parser.add_argument('-p', '--printer', type=str, choices=["table", "csv"], default='table', help="choose the printer type")
+    parser.add_argument('-t', '--table', type=str, help="the table selected for loading data (only works when loading CSV file)")
+    parser.add_argument('database', nargs='?', type=str, help="choose a database")
     return parser.parse_args()
 
 if __name__ == "__main__":
